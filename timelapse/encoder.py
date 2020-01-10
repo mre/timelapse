4e667ab0b53b2d6097e4a9774d6680147deaf9f1
orcutt989 <orcutt989@gmail.com>
2020-01-10 12:37:09 -0500

    add command typing

--- timelapse/encoder.py
+++ timelapse/encoder.py
@@ -41,7 +41,7 @@ class Encoder(Thread):
         """
         # Call ffmpeg with settings compatible with QuickTime.
         # https://superuser.com/a/820137
-        command: = ["/usr/local/bin/ffmpeg", "-y",
+        command: List[str]= ["/usr/local/bin/ffmpeg", "-y",
                    "-framerate", "30",
                    "-i", self.input,
                    "-vf", "format=yuv420p",
